b1f0378addade58e4312754d95a9e9d8c86ede0c
Phoebe Whiteley <phoebex17@gmail.com>
2025-01-02 11:52:35 +0000

    add methods to models

--- blog/models.py
+++ blog/models.py
@@ -16,6 +16,12 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
 
+class Meta:
+    ordering = ["-created_on"]
+
+def __str__(self):
+        return f"{self.title} | written by {self.author}"
+
 class Comment(models.Model):
     post = models.ForeignKey(
         Post, on_delete=models.CASCADE, related_name="comments")
@@ -25,15 +31,10 @@ class Comment(models.Model):
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
 
-class Comment(models.Model):
-    post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name="comments"
-    )
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="commenter"
-    )
-    body = models.TextField()
-    approved = models.BooleanField(default=False)
-    created_on = models.DateTimeField(auto_now_add=True)
+class Meta:
+    ordering = ["-created_on"]
+
+def __str__(self):
+        return f"Comment {self.body} by {self.author}"
 
 
